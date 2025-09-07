6f4ec850e64b02f529743b94b193f2b2d12d7ae2
Jayesh K <jayeshkwal2209@gmail.com>
2025-09-07 20:11:20 +0530

    Delete task77_reverse_string.py

--- task77_reverse_string.py
+++
@@ -1,9 +0,0 @@
-def reverse_string(s):
-    if len(s) <= 1:
-        return s
-    return reverse_string(s[1:]) + s[0]
-
-
-text = input("Enter a string: ")
-reversed_text = reverse_string(text)
-print("Reversed string:", reversed_text)
