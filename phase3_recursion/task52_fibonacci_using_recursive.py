1475ac238d76cfb508ba80e394df503037a96280
Jayesh K <jayeshkwal2209@gmail.com>
2025-09-07 20:07:04 +0530

    Create task52_fibonacci_using_recursive.py

---
+++ phase3_recursion/task52_fibonacci_using_recursive.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Invalid input"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+n = int(input("Enter the position (n): "))
+print(f"Fibonacci number at position {n} is:", fibonacci(n))
