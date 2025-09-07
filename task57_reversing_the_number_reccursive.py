a3fced70f558e03ed0a7db0f6aa6c219068e5983
Jayesh K <jayeshkwal2209@gmail.com>
2025-09-07 20:08:25 +0530

    Delete task57_reversing_the_number_reccursive.py

--- task57_reversing_the_number_reccursive.py
+++
@@ -1,9 +0,0 @@
-def reverse_number(n, rev=0):
-    if n == 0:
-        return rev
-    else:
-        return reverse_number(n // 10, rev * 10 + n % 10)
-
-num = int(input("Enter a number to reverse: "))
-print("Reversed number is:", reverse_number(num))
-
