cd9e472df680f1715bc65e71e2f342754e181d77
Jayesh K <jayeshkwal2209@gmail.com>
2025-09-07 20:08:40 +0530

    Delete task59_sum_of_n_natural_numbers_recursive.py

--- task59_sum_of_n_natural_numbers_recursive.py
+++
@@ -1,8 +0,0 @@
-def rec_sum(n):
-    if n == 0:
-        return 0
-    else:
-        return n + rec_sum(n - 1)
-
-n = int(input("Enter the number whose sum you want: "))
-print("Sum is: " ,rec_sum(n))
